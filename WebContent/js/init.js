566ad3e310a28e7ef02c699cc364f740204b0af0
randreucetti <ross.andreucetti@gmail.com>
2015-03-27 11:41:23 +0800

    New theme

--- WebContent/js/init.js
+++ WebContent/js/init.js
@@ -1,49 +1,55 @@
 /*
-	Big Picture by HTML5 UP
+	Read Only by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
 (function($) {
 
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			'max': { range: '*', href: 'css/style.css', containers: 1440, viewport: { scalable: false }, grid: { gutters: 40 } },
-			'wide': { range: '-1920', href: 'css/style-wide.css', containers: 1360 },
-			'normal': { range: '-1680', href: 'css/style-normal.css', containers: 1200 },
-			'narrow': { range: '-1280', href: 'css/style-narrow.css', containers: 960 },
-			'narrower': { range: '-1000', href: 'css/style-narrower.css', containers: '95%' },
-			'mobile': { range: '-736', href: 'css/style-mobile.css', grid: { gutters: 20 } },
-			'mobile-narrow': { range: '-480', containers: '95%!', grid: { collapse: true } }
+			global: { href: 'css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
+			xlarge: { media: '(max-width: 1680px)', href: 'css/style-xlarge.css' },
+			large: { media: '(max-width: 1280px)', href: 'css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
+			medium: { media: '(max-width: 1024px)', href: 'css/style-medium.css', containers: '85%!' },
+			small: { media: '(max-width: 736px)', href: 'css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
+			xsmall: { media: '(max-width: 480px)', href: 'css/style-xsmall.css' }
+		},
+		plugins: {
+			layers: {
+				config: {
+					mode: 'transform'
+				},
+				titleBar: {
+					breakpoints: 'medium',
+					width: '100%',
+					height: 44,
+					position: 'top-left',
+					side: 'top',
+					html: '<span class="toggle" data-action="toggleLayer" data-args="sidePanel"></span><span class="title" data-action="copyText" data-args="logo"></span>'
+				},
+				sidePanel: {
+					breakpoints: 'medium',
+					hidden: true,
+					width: { small: 275, medium: '20em' },
+					height: '100%',
+					animation: 'pushX',
+					position: 'top-right',
+					side: 'right',
+					orientation: 'vertical',
+					clickToHide: true,
+					html: '<div data-action="moveElement" data-args="header"></div>'
+				}
+			}
 		}
 	});
 
 	$(function() {
 
-		var	$window = $(window),
-			$body = $('body'),
+		var $body = $('body'),
 			$header = $('#header'),
-			$all = $body.add($header);
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 0);
-			});
-
-		// Touch mode.
-			skel.change(function() {
-
-				if (skel.vars.isMobile || skel.isActive('mobile'))
-					$body.addClass('is-touch');
-				else
-					$body.removeClass('is-touch');
-
-			});
+			$nav = $('#nav'), $nav_a = $nav.find('a'),
+			$wrapper = $('#wrapper');
 
 		// Forms (IE<10).
 			var $form = $('form');
@@ -60,190 +66,54 @@
 					$form.n33_formerize();
 				}
 
-				// Custom select.
-					$form.find('.select select')
-						.on('focus', function() {
-							$(this).parent().addClass('focus');
-						})
-						.on('blur', function() {
-							$(this).parent().removeClass('focus');
-						});
-
 			}
 
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+		// Header.
+			var ids = [];
 
-		// Gallery.
-			$('.gallery').poptrox({
-				baseZIndex: 10001,
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#1f2328',
-				overlayOpacity: 0.65,
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: (skel.isActive('mobile') ? 5 : 50),
-				usePopupNav: true
-			});
+			// Set up nav items.
+				$nav_a
+					.scrolly()
+					.on('click', function(event) {
 
-		// Section transitions.
-			if (!skel.vars.isMobile
-			&&	skel.canUseProperty('transition')) {
+						var $this = $(this),
+							href = $this.attr('href');
 
-				var on = function() {
+						// Not an internal link? Bail.
+							if (href.charAt(0) != '#')
+								return;
 
-					// Generic sections.
-						$('.main.style1')
-							.scrollex({
-								mode:		'middle',
-								delay:		100,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								leave:		function() { $(this).addClass('inactive'); }
-							});
+						// Prevent default behavior.
+							event.preventDefault();
 
-						$('.main.style2')
-							.scrollex({
-								mode:		'middle',
-								delay:		100,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								leave:		function() { $(this).addClass('inactive'); }
-							});
+						// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
+							$nav_a
+								.removeClass('active')
+								.addClass('scrollzer-locked');
 
-					// Work.
-						$('#work')
-							.scrollex({
-								top:		'40vh',
-								bottom:		'30vh',
-								delay:		50,
-								initialize:	function() {
+						// Set active class on this link.
+							$this.addClass('active');
 
-												var t = $(this);
+					})
+					.each(function() {
 
-												t.find('.row.images')
-													.addClass('inactive');
+						var $this = $(this),
+							href = $this.attr('href'),
+							id;
 
-											},
-								terminate:	function() {
+						// Not an internal link? Bail.
+							if (href.charAt(0) != '#')
+								return;
 
-												var t = $(this);
+						// Add to scrollzer ID list.
+							id = href.substring(1);
+							$this.attr('id', id + '-link');
+							ids.push(id);
 
-												t.find('.row.images')
-													.removeClass('inactive');
+					});
 
-											},
-								enter:		function() {
-
-												var t = $(this),
-													rows = t.find('.row.images'),
-													length = rows.length,
-													n = 0;
-
-												rows.each(function() {
-													var row = $(this);
-													window.setTimeout(function() {
-														row.removeClass('inactive');
-													}, 100 * (length - n++));
-												});
-
-											},
-								leave:		function(t) {
-
-												var t = $(this),
-													rows = t.find('.row.images'),
-													length = rows.length,
-													n = 0;
-
-												rows.each(function() {
-													var row = $(this);
-													window.setTimeout(function() {
-														row.addClass('inactive');
-													}, 100 * (length - n++));
-												});
-
-											}
-							});
-
-					// Contact.
-						$('#contact')
-							.scrollex({
-								top:		'50%',
-								delay:		50,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								leave:		function() { $(this).addClass('inactive'); }
-							});
-
-				};
-
-				var off = function() {
-
-					// Generic sections.
-						$('.main.style1')
-							.unscrollex();
-
-						$('.main.style2')
-							.unscrollex();
-
-					// Work.
-						$('#work')
-							.unscrollex();
-
-					// Contact.
-						$('#contact')
-							.unscrollex();
-
-				};
-
-				skel.change(function() {
-
-					if (skel.isActive('mobile'))
-						(off)();
-					else
-						(on)();
-
-				});
-
-			}
-
-		// Events.
-			var resizeTimeout, resizeScrollTimeout;
-
-			$window
-				.resize(function() {
-
-					// Disable animations/transitions.
-						$body.addClass('is-resizing');
-
-					window.clearTimeout(resizeTimeout);
-
-					resizeTimeout = window.setTimeout(function() {
-
-						// Update scrolly links.
-							$('a[href^=#]').scrolly({
-								speed: 1500,
-								offset: $header.outerHeight() - 1
-							});
-
-						// Re-enable animations/transitions.
-							window.setTimeout(function() {
-								$body.removeClass('is-resizing');
-								$window.trigger('scroll');
-							}, 0);
-
-					}, 100);
-
-				})
-				.load(function() {
-					$window.trigger('resize');
-				});
+			// Initialize scrollzer.
+				$.scrollzer(ids, { pad: 300, lastHack: true });
 
 	});
 
