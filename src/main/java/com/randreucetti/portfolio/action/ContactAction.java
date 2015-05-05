beba07f5a37843f4bece867214b42f6babd1c551
randreucetti <ross.andrecetti@gmail.com>
2015-05-05 17:48:29 +0800

    Adding struts for contact form

---
+++ src/main/java/com/randreucetti/portfolio/action/ContactAction.java
@@ -0,0 +1,12 @@
+package com.randreucetti.portfolio.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ContactAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1250672710718682278L;
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,6 +4,7 @@
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
+<%@ taglib prefix="s" uri="/struts-tags" %>
 <html>
 <head>
 <title>Ross Andreucetti</title>
